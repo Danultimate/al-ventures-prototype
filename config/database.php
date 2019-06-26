5003c77c3bf065e72adf8f909ef30abf8d6c2a82
Daniel Blanco <dan16.daniel@gmail.com>
2019-06-26 17:56:33 -0500

    database

--- config/database.php
+++ config/database.php
@@ -41,12 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '8889'),
-            'database' => env('DB_DATABASE', 'ecom'),
-            'username' => env('DB_USERNAME', 'test'),
-            'password' => env('DB_PASSWORD', 'root'),
-            'unix_socket' => env('UNIX_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
+            'host' => env('DB_HOST', 'db4free.net'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'ecom_16'),
+            'username' => env('DB_USERNAME', 'idont_care16'),
+            'password' => env('DB_PASSWORD', 'micontrasena0000'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
